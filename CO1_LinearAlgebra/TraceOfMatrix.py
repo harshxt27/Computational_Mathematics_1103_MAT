4af5459948f1ccaac5c430d88bfe8dc18cee5655
harshxt27 <harshit.msj@Gmail.com>
2025-08-13 23:24:48 +0530

    Trace

---
+++ CO1_LinearAlgebra/TraceOfMatrix.py
@@ -0,0 +1,12 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+
+
+#Trace of a matrix.
+A = np.array([[2, 9, 8], [4, 7, 1], [8, 2, 5]])
+A
+Trace_A = np.trace(A)
+Trace_A
+print(Trace_A)
